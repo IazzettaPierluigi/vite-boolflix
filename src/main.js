ae6e57fda2f8602f0d98529304e4b8347530909c
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-02-13 11:21:15 +0100

    initial changes and bootstrap config

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Import our custom CSS
+import './styles/general.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+
 createApp(App).mount('#app')
